b27c9a5628f44e13701a6f41d59a0115e4550280
mrdangg <mrdang322002@gmail.com>
2021-11-30 15:42:26 +0700

    lan2

--- js/music-list.js
+++ js/music-list.js
@@ -1,70 +1,78 @@
-// To add more song, just copy the following code and paste inside the array
-
-//   {
-//     name: "Here is the music name",
-//     artist: "Here is the artist name",
-//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-//   }
-
-//paste it inside the array as more as you want music then you don't need to do any other thing
 
 let allMusic = [
   {
-    name: "Harley Bird - Home",
-    artist: "Jordan Schor",
+    name: "Có hẹn với thanh xuân ",
+    artist: "Monstar x AnhVu",
     img: "music-1",
     src: "music-1"
   },
   {
-    name: "Ikson Anywhere – Ikson",
-    artist: "Audio Library",
+    name: "Badboy",
+    artist: "NIZ x CilTee",
     img: "music-2",
     src: "music-2"
   },
   {
-    name: "Beauz & Jvna - Crazy",
-    artist: "Beauz & Jvna",
+    name: "Đường Tôi Chở Em Về ",
+    artist: "Buitruonglinh「Cukak Remix」",
     img: "music-3",
     src: "music-3"
   },
   {
-    name: "Hardwind - Want Me",
-    artist: "Mike Archangelo",
+    name: "Ai Chung Tình Được Mãi ",
+    artist: "Đinh Tùng Huy「Cukak Remix」",
     img: "music-4",
     src: "music-4"
   },
   {
-    name: "Jim - Sun Goes Down",
-    artist: "Jim Yosef x Roy",
+    name: "Tình Yêu Của Anh ",
+    artist: " Phú Quí「Cukak Remix」",
     img: "music-5",
     src: "music-5"
   },
   {
-    name: "Lost Sky - Vision NCS",
-    artist: "NCS Release",
+    name: "Đi Về Nhà",
+    artist: "Đen x JustaTee",
     img: "music-6",
     src: "music-6"
   },
   {
-    name: "Warriyo - Mortals",
-    artist: "NCS Release",
+    name: "Làm gì phải Hốt",
+    artist: "Đen ",
     img: "music-7",
     src: "music-7"
   },
   {
-    name: "Unknown Brain - MATAFAKA",
-    artist: "NCS Release",
+    name: "Tết đong đầy",
+    artist: "Kay Trần ",
     img: "music-8",
     src: "music-8"
   },
   {
-    name: "Adventure",
-    artist: "NCS Release",
+    name: "last christmas",
+    artist: "George Michael & Wham",
     img: "music-9",
     src: "music-9"
   },
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
   // like this paste it and remember to give comma after ending of this bracket }
   // {
   //   name: "Here is the music name",
