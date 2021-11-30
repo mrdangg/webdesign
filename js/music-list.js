167faa650e0f92ab6463b68abdbecea883c3f8b2
mrdangg <mrdang322002@gmail.com>
2021-11-30 19:52:44 +0700

    lan5

--- js/music-list.js
+++ js/music-list.js
@@ -1,59 +1,11 @@
 
 let allMusic = [
   {
-    name: "Có hẹn với thanh xuân ",
-    artist: "Monstar x AnhVu",
+    name: "happy brithday",
+    artist: "Dũng",
     img: "music-1",
     src: "music-1"
-  },
-  {
-    name: "Badboy",
-    artist: "NIZ x CilTee",
-    img: "music-2",
-    src: "music-2"
-  },
-  {
-    name: "Đường Tôi Chở Em Về ",
-    artist: "Buitruonglinh「Cukak Remix」",
-    img: "music-3",
-    src: "music-3"
-  },
-  {
-    name: "Ai Chung Tình Được Mãi ",
-    artist: "Đinh Tùng Huy「Cukak Remix」",
-    img: "music-4",
-    src: "music-4"
-  },
-  {
-    name: "Tình Yêu Của Anh ",
-    artist: " Phú Quí「Cukak Remix」",
-    img: "music-5",
-    src: "music-5"
-  },
-  {
-    name: "Đi Về Nhà",
-    artist: "Đen x JustaTee",
-    img: "music-6",
-    src: "music-6"
-  },
-  {
-    name: "Làm gì phải Hốt",
-    artist: "Đen ",
-    img: "music-7",
-    src: "music-7"
-  },
-  {
-    name: "Tết đong đầy",
-    artist: "Kay Trần ",
-    img: "music-8",
-    src: "music-8"
-  },
-  {
-    name: "last christmas",
-    artist: "George Michael & Wham",
-    img: "music-9",
-    src: "music-9"
-  },
+  }
 
 
 
