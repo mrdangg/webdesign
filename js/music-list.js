14fc7deb3c5af47a34d58600bde7c3a44268145e
mrdangg <mrdang322002@gmail.com>
2021-12-01 21:06:45 +0700

    lan5

--- js/music-list.js
+++ js/music-list.js
@@ -1,11 +1,60 @@
 
 let allMusic = [
   {
-    name: "happy brithday",
-    artist: "Dũng",
+    name: "Why Not Me",
+    artist: "Enrique Iglesias",
     img: "music-1",
     src: "music-1"
-  }
+  },
+  {
+    name: "Bad Boy",
+    artist: "NIZ x AnhVu「Remix Ver. by 1 9 6 7」",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Đường tôi chở em về",
+    artist: "Buitruonglinh「Cukak Remix」",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Ai Chung tình được mãi",
+    artist: "Đinh Tùng Huy「Cukak Remix」",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Tình Yêu Của Anh",
+    artist: "Phú Quí「Cukak Remix」",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Đường về nhà",
+    artist: "Đen",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Làm gì mà phải hốt",
+    artist: "ustaTee x Hoàng Thùy Linh x Đen",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Tết đong đầy",
+    artist: "Kay trần",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "last christmas",
+    artist: "George Michael & Wham",
+    img: "music-9",
+    src: "music-9"
+  },
+
 
 
 
